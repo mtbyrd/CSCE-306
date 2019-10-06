8183636b0960be68768d216f201f9e5918839c2a
mtbyrd <55217610+mtbyrd@users.noreply.github.com>
2019-10-05 19:13:12 -0500

    Add files via upload

--- HW3.cpp
+++ HW3.cpp
@@ -1,8 +1,8 @@
 //============================================================================
 // Name        : HW3.cpp
-// Author      : Tim and Michaela
+// Author      : Michaela Byrd
 // Version     :
-// Copyright   : Your copyright notice
+// Copyright   : Description:
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -14,6 +14,7 @@
 
 using namespace std;
 
+
 // You will need these forward references.
 class Expr;
 class Stmt;
@@ -24,7 +25,7 @@ vector<string> lexemes;
 vector<string> tokens;
 vector<string>::iterator lexitr;
 vector<string>::iterator tokitr;
-map<string, int> vartable; 	// map of variables and their values- x=5
+map<string, int> vartable; 	// map of variables and their values
 vector<Stmt *> insttable; 		// table of instructions
 map<string, string> symboltable; // map of variables to datatype (i.e. sum t_integer)
 
@@ -47,6 +48,16 @@ public:
 	virtual ~Stmt(){};
 	virtual string toString() = 0;
 	virtual void execute() = 0;
+	string getName (){
+	//pre:
+	//post: name member variable is returned
+		return name;
+	}
+	void setName (string s){
+	//pre: the parameter is the name member variable
+	//post: name has been set to s
+		name = s;
+	}
 };
 
 class AssignStmt : public Stmt{
@@ -64,10 +75,22 @@ class InputStmt : public Stmt{
 private:
 	string var;
 public:
-	InputStmt(string vname);
+	InputStmt(string vname){
+		cout << "Constructing an InputStmt object" << endl;
+		Stmt::setName("t_input");
+		var = vname;
+	}
 	~InputStmt();
-	string toString();
-	void execute();
+	string toString(){
+		return "Name: " + Stmt::getName() + " Variable Name: " + var;
+	}
+	void execute(){
+		string val;
+		//Assuming I don't need to do anything else specific for this
+		cout << "Enter value for of type " << symboltable[var]<< "for: " << var << endl;
+		cin >> val;
+		vartable[var] = val;
+	}
 };
 class StrOutStmt : public Stmt{
 private:
@@ -83,10 +106,22 @@ class ExprOutStmt : public Stmt{
 private:
 	Expr* p_expr;
 public:
-	ExprOutStmt();
-	~ExprOutStmt();
-	string toString();
-	void execute();
+	ExprOutStmt(Expr* e){
+		cout << "Constructing ExprOutStmt" << endl;
+		p_expr = e;
+	}
+	~ExprOutStmt(){
+		cout << "Deconstructing ExprOutStmt" << endl;
+		delete (p_expr);
+	}
+	string toString(){
+		cout << "COnverting ExprOutStmt to string" << endl;
+		return "Name: " + Stmt::getName() + " Expression: " + p_expr;
+	}
+	void execute(){
+		cout << "Executing ExprOutStmt" << endl;
+		cout << p_expr->eval() << endl;
+	}
 };
 
 class IfStmt : public Stmt{
@@ -96,6 +131,7 @@ private:
 public:
 	IfStmt();
 	~IfStmt();
+	void setElseTarget (int);
 	string toString();
 	void execute();
 };
@@ -103,18 +139,61 @@ public:
 class WhileStmt : public Stmt{
 private:
 	Expr* p_expr;
-	//The line to go to after the condition fails
 	int elsetarget;
 public:
-	WhileStmt(vector<int> exprs, vector<string> opers, int go2);
-	WhileStmt();
-	~WhileStmt();
-	string toString();
-	void execute();
-	void setTarget(int x);
-	/*Pre: The parameter is the instruction number of the next instruction to execute after the while condition is not met
-	 *Post: elsetarget is set to the instruction number of the next instruction to execute after the while condition is not met
-	 */
+	WhileStmt(Expr* e){
+		cout << "Constructing a WhileStmt object" << endl;
+		Stmt::setName("t_while");
+		p_expr = e;
+		elsetarget = -1;
+	}
+	~WhileStmt(){
+		cout << "Destructing a WhileStmt object" << endl;
+		delete(p_expr);
+	}
+	string toString(){
+		cout << "Converting a WhileStmt object to string" << endl;
+		return "Name: " + Stmt::getName() + " Condition: " + p_expr + " Target: " + elsetarget;
+	}
+	void setTarget(int x){
+	//pre: The parameter is the index of the next instruction to execute after the while loop fails its condition.
+	//post: elsetarget is set to x
+		cout << "Setting elsetarget in WhileStmt" << endl;
+		elsetarget = x;
+	}
+	void execute(){
+		cout << "Executing a WhileStmt" << endl;
+		//pc is set to the start of the while loop coming in
+		while (p_expr->eval()){
+			//Assumes that pc is incremented in eval, else increment pc here
+			while (pc<elsetarget){
+				insttable[pc]->execute();
+				pc++;
+			}
+		}
+	}
+};
+
+class GoToStmt : public Stmt{
+private:
+	int gotoline;
+public:
+	GoToStmt(int x){
+		//pre: constructs a statement that instructs program to go to a certain instruction. Parameter is the index of that instrucion in insttable
+		//post: object is created
+		cout << "Constructing a GoToStmt" << endl;
+		Stmt::setName("t_end");
+		gotoline = x;
+	}
+	~GoToStmt(){cout << "Deconstructing a GoToStmt" << endl;}
+	string toString (){
+		cout << "Converting a GoToStmt to string" << endl;
+		return "Name: " + Stmt::getName() + " Going to: " + gotoline;
+	}
+	void execute (){
+		cout << "Executing a GoToStmt" << endl;
+		pc = gotoline;
+	}
 };
 
 
@@ -153,27 +232,45 @@ private:
 	vector<Expr *> exprs;
 	vector<string> ops;  // tokens of operators
 public:
-	InFixExpr();
-	//populates infix expression in a default way
-	InFixExpr(vector<int> nums, vector<string> opers);
-	//populate infix expression with values
 	~InFixExpr();
-	//might want overload for boolean expressions
-	//bool eval();
-	//int eval();
+	int eval();
 	string toString();
 };
 
 class Compiler{
 private:
-	IfStmt buildIf();
+	IfStmt* buildIf();
 	WhileStmt* buildWhile();
-	void buildStmt();
-	void buildAssign();
-	void buildInput();
-	void buildOutput();
+	Stmt* buildStmt();
+	AssignStmt* buildAssign();
+	InputStmt* buildInput();
+	Stmt* buildOutput();
 	// use one of the following buildExpr methods
-	void buildExpr(Expr*&);      Expr* buildExpr();
+	void buildExpr(Expr*&);
+	Expr* buildExpr(){
+		//Assumes that iterators are pointing directly to the start of an expression
+		vector <int> nums;
+		vector <string> ops;
+		while (*tokitr!="s_rparen" && *tokitr!="s_semi"){
+			//Analyzing a new lexeme
+			string num = "";
+			string str = *lexitr;
+			std::string::const_iterator stritr = str.begin();
+			if (isdigit(*stritr)){
+				while (isdigit(*stritr)){
+					//construct the first number
+					num += *stritr;
+					stritr++;
+				}
+				nums.push_back(stoi(num));
+			}
+			else {
+				ops.push_back(*lexitr);
+			}
+			tokitr++; lexitr++;
+		}
+
+	}
 	void populateTokenLexemes(istream& infile);
 	void populateSymbolTable(istream& infile);
 public:
@@ -181,160 +278,66 @@ public:
 		populateTokenLexemes(source);
 		populateSymbolTable(symbols);
 	}
-	bool compile();  	// builds the instruction table and declaration check
-	void run();  		// executes the instruction table
-
-};
-
-//Michaela wrote the following methods for the WhileStmt Class
-WhileStmt::WhileStmt(){
-	cout << "Default constructing a WhileStmt object" << endl;
-	name = "t_while";
-	//What do the parameters look like coming in?
-	p_expr = new InFixExpr();
-	elsetarget = -1;
-	//when compiling, the program will use a stack to determine which end loop belongs to this and change elsetarget to the next instruction
-}
-
-void WhileStmt::WhileStmt(vector<int> exprs, vector<string> opers, int go2){
-	//object instntiated with its instruction number as the elsetarget variable so it is easy for go_to to access the instruction number it needs
-	cout << "3-arg constructing a WhileStmt object" << endl;
-	name = "t_while";
-	//What do the parameters look like coming in?
-	p_expr = new InFixExpr(exprs, opers);
-	elsetarget = go2;
-	//when compiling, the program will use a stack to determine which end loop belongs to this and change elsetarget to the next instruction
-}
-
-//check against class code
-WhileStmt::~WhileStmt(){
-	cout << "Destructing WhileStmt object" << endl;
-	delete(p_expr);
-}
-
-string WhileStmt::toString(){
-	cout << "Converting WhileStmt to string" << endl;
-	return name + " " + p_expr->toString() + " " + elsetarget;
-}
-
-void WhileStmt::setTarget(int x){
-	cout << "Setting elsetarget in WhileStmt" << endl;
-	elsetarget = x;
-}
-
-void WhileStmt::execute(){
-	//assumes that pc is incremented in eval
-	cout << "Executing WhileStmt" << endl;
-	while (p_expr->eval()){
-		while (pc<elsetarget){
-			insttable[pc]->execute();
-			pc ++;
-		}
-	}
-	pc = elsetarget;
-}
-
-Compiler::Compiler(istream& source, istream& symbols){
-	//populate vectors
-}
-
-//Michaela wrote the method compile
-bool Compiler::compile(){
-	stack <WhileStmt*> open_loops;
-	while (*tokitr!=tokens.end()){
-		if (*tokitr=="t_while"){
-			tokitr++; lexitr++;
-			//what to do to push a pointer?
-			insttable.push_back(buildWhile());
-			//built without exit line - elsetarget is the index of the whilestmt
-			//nvalid arguments 'Candidates are:void push(const WhileStmt &)void push(WhileStmt &&)
-			//Push pointer to while object onto stack
-			open_loops.push(insttable[insttable.size()-1]);
-		}
-		if (*tokitr=="t_end"){
-			tokitr++; lexitr++;
-			if (*tokitr=="t_loop"){
-				insttable.push_back(new GoTo(open_loops.pop()));
+	bool compile(){
+		// builds the instruction table and declaration check
+		stack <int> open_stmts;
+		while (*tokitr!=tokens.end()){
+			if (*tokitr=="t_while"){
+				tokitr++; lexitr++;
+				open_stmts.push(insttable.size());
+				insttable.push_back(buildWhile());
+			}
+			else if (*tokitr=="t_if"){
+				tokitr++; lexitr++;
+				open_stmts.push(insttable.size());
+				insttable.push_back(buildIf());
+			}
+			else if (*tokitr=="t_id"){
+				//Uses current index of lexitr so does not increment here- increments in buildAssign
+				insttable.push_back(buildAssign());
+			}
+			else if (*tokitr=="t_input"){
+				tokitr++; lexitr++;
+				insttable.push_back(buildInput());
+				//this is incremented to the start of the parens
+			}
+			else if (*tokitr=="t_output"){
+				tokitr++; lexitr++;
+				insttable.push_back(buildOutput());
+			}
+			//safe to assume that the final t_end is going to be tokens.end?
+			else if (*tokitr=="t_end"){
+				tokitr++; lexitr++;
+				int line = open_stmts.pop();
+				if (*tokitr=="t_loop"){
+					WhileStmt* ws = dynamic_cast<WhileStmt*>(insttable[line]);
+					if (ws!=nullptr){
+						ws->setTarget(insttable.size());
+					}
+					//Need condition for failed cast?
+					insttable.push_back(new GoToStmt(line));
+				}
+				else{
+					// t_if
+					IfStmt* is = dynamic_cast<IfStmt*>(insttable[line]);
+					if (is!=nullptr){
+						is->setElseTarget(insttable.size());
+					}
+				}
+				tokitr++; lexitr++;
+				//incremented to start of new instructions
 			}
 		}
-		//read in statements, add to insttable
-		//when end loop is found, add to insttable, inc ctr, pop while loop off of stack, and set elsetarget to ctr
-		else if (*tokitr=="t_if"){
-			insttable.push_back(buildIf());
-		}
-		else if (*tokitr=="t")
-		tokitr++; lexitr++;
 	}
-}
-
-WhileStmt* Compiler::buildWhile(){
-	//iterate token and lexeme map to get past the parentheses
-	tokitr++; lexitr++;
-	vector<int> exprs;
-	vector<string> ops;
-	if (*tokitr=="t_id"){
-		exprs.push_back(vartable[*tokitr]);
-	}
-	else{
-		exprs.push_back(stoi(*lexitr));
-	}
-	tokitr++; lexitr++;
-	ops.push_back(*tokitr);
-	tokitr++; lexitr++;
-	if (*tokitr=="t_id"){
-		exprs.push_back(vartable[*tokitr]);
-	}
-	else{
-		exprs.push_back(stoi(*lexitr));
-	}
-	//increment past the already-read lexeme
-	//increment past the rparen
-	//increment past the loop
-	//iterators should be at the start of the next line
-	tokitr = tokitr + 3; lexitr = lexitr + 3;
-	//Symbol WhileStmt could not be resolved- why?
-	return new WhileStmt (exprs, ops, insttable.size());
-}
+	void run();  		// executes the instruction table
+};
 
 
-
-
-
-
-
-//Michaela wrote the following methods for InputStmt
-//Planning to write compile to pass the appropriate information to InputStmt
-InputStmt::InputStmt(string vname){
-	name = "t_input";
-	var = vname;
-}
-//Delete nothing?
-InputStmt::~InputStmt(){}
-
-string InputStmt::toString(){
-	return name + " " + var + " " + vartable[var];
-}
-
-void InputStmt::execute(){
-	//Determine data type?
-	//Do I need to call assignment stmt?
-	string val;
-	cout << "Enter a value for " << var << endl;
-	cin >> val;
-	vartable [var] = val;
-}
 
 int main() {
-	cout << "Arugula" << endl; // prints Arugula
+        cout << "Arugula" << endl; // prints Arugula
 	return 0;
 }
 
 
 
-
-
-
-IdExpr::IdExpr(string s){
-
-
-}
